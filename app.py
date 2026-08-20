4f195ba134c662186404a5325f9f23a12dca036f
Rijkaaard <fiordalisi.riccardo8@gmail.com>
2026-08-20 11:09:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -471,7 +471,7 @@ if not is_tv_mode:
                     else:
                         st.info(f"**Ruolo:** {info_g['Ruolo']} | **Squadra Serie A:** {squadra_serie_a} | **Prezzo Medio:** {int(info_g['Prezzo_Numerico'])}")
 
-            tab_svincola:
+            with tab_svincola:
                 if st.session_state.acquisti:
                     col_del_g, col_del_btn = st.columns([3, 1])
                     with col_del_g:
